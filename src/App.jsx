8883bc19490d67d863f3ca25085c8b014cefe1b8
Ajarek <ajarek@poczta.onet.pl>
2023-03-31 01:02:40 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className='App'>
+      <div className='aside'>
+        <button>+ New chat</button>
+      </div>
+      <div className='section'>
+        <h1>ChatGPT</h1>
+        <p>
+          As an AI language model, I don't have a physical presence or use fonts
+          like humans do. However, the font used to display my responses on a
+          website or interface can be set by the website or application
+          developer using CSS styles. The font family used for ChatGPT's
+          responses would depend on the specific implementation and design
+          choices made by the developer.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
