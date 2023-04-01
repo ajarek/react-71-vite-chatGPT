7bd1d530fbd68c2bb6783189d8b2655ae105196d
Ajarek <ajarek@poczta.onet.pl>
2023-04-01 21:32:12 +0200

    content added to Aside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { FiSun,FiLogOut,FiPlus,FiUser } from 'react-icons/fi';
+import { RiDeleteBinLine } from 'react-icons/ri';
+import { HiOutlineExternalLink } from 'react-icons/hi';
 const Aside = () => {
   return (
     <aside className='aside'>
-    <button>+ New chat</button>
+    
+    <div className="history">
+    <button><FiPlus/> New chat</button>
+    </div>
+    <div className="setting">
+      <button><RiDeleteBinLine/> Clear Conversation</button>
+      <button><FiUser/> Upgrade to Plus</button>
+      <button><FiSun/> Light mode</button>
+      <button><HiOutlineExternalLink/> Update & FAQ</button>
+      <button><FiLogOut/> Log out</button>
+    </div>
   </aside>
   )
 }
